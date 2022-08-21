ac4add556ad8b2aa1b26982315cf82e928f11fcf
grantgariepy <grantgariepy@protonmail.com>
2022-08-21 15:31:26 -0600

    very little progress with spotify logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,15 +6,34 @@ source: https://sketchfab.com/3d-models/apple-iphone-13-pro-max-4328dea00e47497d
 title: Apple iPhone 13 Pro Max
 */
 
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import Mesh from '@react-three/fiber'
-import { Euler, Vector3, Color } from '@react-three/fiber'
+import { useThree } from '@react-three/fiber'
+import { EffectComposer } from "postprocessing";
+import { RenderPass } from "postprocessing";
+// import  UnrealBloomPass  from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+
 
 type ModelType = {
   [key: string]: any;
 }
+
+// const { scene, gl, size, camera } = useThree();
+
+// const aspect = useMemo(() => new THREE.Vector2(size.width, size.height), [
+//   size,
+// ]);
+
+// const bloom = {
+//   resolution: aspect,
+//   strength: 0.6,
+//   radius: 0.01,
+//   threshold: 0.4,
+// };
+
+
 
 export default function Model({ ...props }) {
 
@@ -22,20 +41,25 @@ export default function Model({ ...props }) {
   const group = React.useRef();
 
   
+  
 
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime();
     group.current.rotation.y = -a*.5
+   
   });
 
-  const { nodes, materials }: ModelType = useGLTF("/green.gltf");
+  
+
+  const { nodes, materials }: ModelType = useGLTF("/logo.gltf");
   return (
-    <group ref={group} {...props} dispose={null}>
+    <group ref={group} {...props}  dispose={null}>
       <group position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0.5, 0]} scale={0.00003}>
-          <group scale={500}>
-            <mesh geometry={nodes.Mesh_0.geometry} material={materials['Material.003']} clearcoat={0.8} envMapIntensity={0.4}clearcoatRoughness={0} roughness={1} metalness={0}/>
-            <mesh geometry={nodes.Mesh_1.geometry} material={materials['Material.002']} position={[0, 0, -7.37]} clearcoat={0.8} envMapIntensity={0.4}clearcoatRoughness={0} roughness={1} metalness={0}/>
+        <group rotation={[Math.PI / 2, 0.5, 0]} scale={0.00003} >
+          <group scale={500} >
+          <mesh   
+            geometry={nodes.Mesh_1.geometry} 
+            material={materials['Material.001']}/>
             
           </group>
         </group>
@@ -44,4 +68,4 @@ export default function Model({ ...props }) {
   );
 }
 
-useGLTF.preload("/green.gltf");
+useGLTF.preload("/logo.gltf");
