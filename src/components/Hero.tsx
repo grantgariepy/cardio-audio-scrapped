65faf4c3b7f1f037e2faf797a70bba8c81c9c583
grantgariepy <grantgariepy@protonmail.com>
2022-08-21 20:56:15 -0600

    small progress getting the token

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <>
       <Canvas className="canvas"  camera={{ position: [0, 0, 5], fov: 45 }}>
-        <OrbitControls enableZoom={true} />
+        
         <spotLight intensity={0.5} angle={0.2} penumbra={1} position={[5, 15, 10]} />
         <Suspense fallback={null}>
           
