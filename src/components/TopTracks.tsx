73c0c3b041e43d8c4aff0fbc0f9de9537ac72e9b
grantgariepy <grantgariepy@protonmail.com>
2022-08-27 07:58:34 -0600

    format

--- src/components/TopTracks.tsx
+++ src/components/TopTracks.tsx
@@ -27,8 +27,6 @@ export default function TopTracks() {
                 
                 <h1 className='title'>{item.album.artists[0].name} - </h1>
                 <h1 className='title'>{item.album.name}</h1>
-                {/* <h1 className='title'>{item.images[0].url}</h1> */}
-                {/* <h1 className='title'>{item.images[1].height}</h1>*/}
                 <img src={item.album.images[0].url} width='100' /> 
               </div>
             </div>
